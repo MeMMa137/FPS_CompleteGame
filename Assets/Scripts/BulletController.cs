8ab8d5a243590b5d2029ddaa42501b7d2d513ea9
Memma <emmaborghi1@gmail.com>
2023-10-09 20:58:59 +0200

    lz 41
    aggiunto:
    -nemico spara
    -tempi di reload e visione del nemico limitata
    -bozza della skin nemico

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -12,6 +12,8 @@ public class BulletController : MonoBehaviour
 
     public int damage = 1;
 
+    public bool damageEnemy, damagePlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,9 +35,14 @@ public class BulletController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other) //quando l'oggetto associato a questo script colliderà con un altro oggetto, allora si attiverà la funizone
     {
-        if(other.gameObject.tag == "Enemy") //se il proiettile colpisce un oggetto di tipo "Enemy"
+        if(other.gameObject.tag == "Enemy" && damageEnemy) //se il proiettile colpisce un oggetto di tipo "Enemy" e ha abilitata la funzione di "damageEnemy"
         {
-            other.gameObject.GetComponent<EnemyHealthController>().DamageEnemy(damage);//allora sarà chiamata la funzione per togliere vita al nemico (DamageEnemy) nella classe "EnemyHealthController"
+            other.gameObject.GetComponent<EnemyHealthController>().DamageEnemy(damage); //allora sarà chiamata la funzione per togliere vita al nemico (DamageEnemy) nella classe "EnemyHealthController"
+        }
+
+        if(other.gameObject.tag == "Player" && damagePlayer) //se il proiettile colpisce un oggetto di tipo "Player" e ha abilitata la funzione di "damagePlayer"
+        {
+            Debug.Log("sta colpendo il giocatore a " + transform.position);
         }
 
         Destroy(gameObject); //distrugge QUESTO oggetto, ovvero il proiettile
--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,8 +5,7 @@ using UnityEngine.AI;
 
 public class EnemyController : MonoBehaviour
 {
-    public float moveSpeed;
-    public Rigidbody theRB;
+    
     private bool chasing; //chi sta seguendo
     public float distanceToChase = 10f, distanceToLose = 15f, distanceToStop = 2f; //distanza per seguire, distanza per non seguire più, distanza da mantenere tra player e nemico
     private Vector3 targetPoint, startPoint;
@@ -16,11 +15,19 @@ public class EnemyController : MonoBehaviour
     public float keepChasingTime = 5f; //tempo default per arrivare all'ultima posizione del player
     private float chaseCounter; //conta alla rovescia il tempo per arrivare all'ultima posizione del player
 
+    public GameObject bullet;
+    public Transform firePoint;
+
+    public float fireRate, waitBetweenShots=2f, timeToShoot=1f; //tempo tra un colpo e l'altro del nemico statico di default, conto alla rovescia sul tempo disponibile per sparare (tra un colpo e l'altro) statico di default, tempo statico di default per "ricaricare" colpi
+    private float fireCount, shotWaitCounter, shootTimeCounter;//tempo tra un colpo e l'altro del nemico, conto alla rovescia sul tempo disponibile per sparare (tra un colpo e l'altro), tempo per "ricaricare" colpi
 
 
     void Start()
     {
         startPoint = transform.position;
+
+        shootTimeCounter = timeToShoot;
+        shotWaitCounter = waitBetweenShots;
     }
 
 
@@ -34,6 +41,9 @@ public class EnemyController : MonoBehaviour
             if (Vector3.Distance(transform.position, targetPoint) < distanceToChase) //se la distanza tra il nemico e il player è minore della distanza minima per poter essere seguito
             {
                 chasing = true; //attiva l'inseguimento
+
+                shootTimeCounter = timeToShoot; //resetta il tempo di ricarica colpi
+                shotWaitCounter = waitBetweenShots;//resetta l'intervallo tra spari
             }
 
             if (chaseCounter > 0) //se tempo non scaduto
@@ -71,6 +81,61 @@ public class EnemyController : MonoBehaviour
 
                 chaseCounter = keepChasingTime; //resetta il conto alla rovescia 
             }
+
+            if(shotWaitCounter > 0) //se si trova quindi nel range partirà lo shotWaitCounter
+            {
+                shotWaitCounter -= Time.deltaTime; //inizia il conto alla rovescia togliendo secondi all'intervallo tra un colpo e l'altro
+
+                if (shotWaitCounter <= 0) //se il tempo di intervallo per sparare finisce
+                {
+                    shootTimeCounter = timeToShoot;//si resetta il valore del tempo
+                }
+            }
+            else
+            {
+
+            
+
+            shootTimeCounter -= Time.deltaTime;
+
+            if(shootTimeCounter > 0)
+            {
+
+            
+
+            fireCount -= Time.deltaTime; //abbassa di 1 al sec il count (su 60fps)
+            if (fireCount <= 0) //intervallo per sparare finito
+            {
+                fireCount = fireRate; //resetta il count
+
+                firePoint.LookAt(PlayerController.instance.transform.position + new Vector3(0f, 1.2f, 0f)); //sposta la visuale del nemico leggermente piu in alto, perche di base guarderebbe il blocco collegato al ground
+
+                        //controlla l'angolo tra la visione del nemico e la posizione del player
+                        Vector3 targetDir = PlayerController.instance.transform.position - transform.position; //sottrae la posizione del giocatore a quella del nemico
+                        float angle = Vector3.SignedAngle(targetDir, transform.forward, Vector3.up);//calcola l'angolo (prende la posizione del player e la sua, ed indica l'asse, calcolandone la differenza)
+
+                        if(Mathf.Abs(angle) < 30f) //se l'angolo tra il player e il nemico è minore di 30 gradi (Abs = anche se negativo, viene percepito come positivo)
+                        {
+                            Instantiate(bullet, firePoint.position, firePoint.rotation); //crea una copia del proiettile nella pistola
+                        }
+                        else
+                        {
+                            shotWaitCounter = waitBetweenShots; //aspetta a sparare
+                        }
+
+                        
+            }
+
+                    agent.destination = transform.position; //quando sta sparando, il nemico non si muoverà
+
+            }
+            else
+            {
+                shotWaitCounter = waitBetweenShots;
+            }
+
+           }
+
         }
 
         
